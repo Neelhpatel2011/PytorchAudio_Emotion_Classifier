3252c28918ec5829418f343e2e0a2b86fea11f10
Neel Hitesh Patel <neelhpatel2011@gmail.com>
2024-11-12 15:37:14 -0500

    Create feature_extraction.py

---
+++ Neel Local Code/feature_extraction.py
@@ -0,0 +1,52 @@
+
+# Import packages!
+
+import os
+import shutil
+from zipfile import ZipFile
+import time
+import random
+from tqdm import tqdm
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import scipy
+import librosa
+
+from IPython.display import Audio
+
+import torch
+from torch.utils.data import Dataset, DataLoader, Subset
+import torchaudio
+from torch.nn.utils.rnn import pad_sequence
+import torch.nn.functional as F
+import torchaudio.transforms as T
+from sklearn.model_selection import train_test_split
+
+import torch.multiprocessing as mp
+mp.set_start_method("spawn", force=True)
+
+#Use GPU acceleration if possible
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+   
+print(f'Using {device}') 
+
+# Set seeds for reproducibility
+SEED = 42
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)  # If using CUDA
+
+AUGMENTATIONS_LOADED = False
+
+
+
+
